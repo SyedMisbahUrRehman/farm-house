e22fbfecddcf2e2b82019add2853352d19061ac6
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2025-03-06 11:52:17 +0500

    icon updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaTwitter } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaMapMarkerAlt } from 'react-icons/fa'
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
@@ -14,10 +15,10 @@ export default function Footer() {
                         <p className="text-sm sm:text-base">123 Farmstead Lane, Rural Valley, USA</p>
                     </div>
                 </div>
-                <div className="flex space-x-6 text-xl sm:text-2xl">
+                <div className="flex space-x-4 text-xl sm:text-2xl">
                     <a href="https://instagram.com/" className="hover:text-green-300 transition-colors"><FaInstagram /></a>
                     <a href="https://facebook.com/" className="hover:text-green-300 transition-colors"><FaFacebook /></a>
-                    <a href="https://x.com/" className="hover:text-green-300 transition-colors"><FaTwitter /></a>
+                    <a href="https://x.com/" className="hover:text-green-300 transition-colors"><FaSquareXTwitter /></a>
                 </div>
             </div>
             <div className=" mx-auto px-4 pt-8  text-center text-sm text-white/80">
